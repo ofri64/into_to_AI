82f114fae321b11921259da8d60a914f34f48299
Ofri Kleinfed <ofri64@gmail.com>
2018-11-10 12:10:57 +0200

    add input file parser

--- src/InputParser.java
+++ src/InputParser.java
@@ -1,2 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class InputParser {
+
+    private String inputFilePath;
+    private int algorithmType;
+    private int boardSize;
+    private int[] initialState;
+
+    public InputParser(String inputFilePath){
+        this.inputFilePath = inputFilePath;
+    }
+
+    public int getAlgorithmType() {
+        return algorithmType;
+    }
+
+    public int getBoardSize() {
+        return boardSize;
+    }
+
+    public int[] getInitialState() {
+        return initialState;
+    }
+
+    public void parseInputFile() {
+        try(BufferedReader br = new BufferedReader(new FileReader(this.inputFilePath))) {
+            this.algorithmType = Integer.parseInt(br.readLine());
+            this.boardSize = Integer.parseInt(br.readLine());
+            String initialStateRaw = br.readLine();
+            String[] stateTokens = initialStateRaw.split("-");
+            this.initialState = new int[boardSize*boardSize];
+
+            for(int i=0; i<stateTokens.length; i++){
+                this.initialState[i] = Integer.parseInt(stateTokens[i]);
+            }
+        }
+        catch (Exception e){
+            System.out.println("The following exception occurred" + e.getMessage());
+        }
+    }
 }
--- src/java_ex1.java
+++ src/java_ex1.java
@@ -1,8 +1,14 @@
 public class java_ex1 {
     public static void main(String[] args) {
-        int n = 3;
-        int[] initialArray = {1, 0, 3, 4, 2, 5, 7, 8, 6};
-        SearchAlgorithmInterface algorithm = AlgorithmsFactory.runSearchAlgorithm(n, initialArray, 2);
+        String inputFilePath = args[0];
+        InputParser parser = new InputParser(inputFilePath);
+        parser.parseInputFile();
+
+        int algoType = parser.getAlgorithmType();
+        int n = parser.getBoardSize();
+        int[] initialArray = parser.getInitialState();
+
+        SearchAlgorithmInterface algorithm = AlgorithmsFactory.runSearchAlgorithm(n, initialArray, algoType);
         System.out.println(algorithm.getSolutionPath() + " " + algorithm.getNumSearchedNodes() + " " + algorithm.getSolutionCost());
 
         int[] initialArray2 = {1, 0, 3, 4, 2, 6, 7, 5, 8};
